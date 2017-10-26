b156315e7f814b5ec392cd33c178807078e9e36a
Anderson Lopes <anderson.Lopes@adtalem.com>
2017-10-26 15:54:11 -0300

    First commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
+export class Hero {
+  id: number;
+  name: String;
+}
+
+const HEROES: Hero[] = [
+  { id: 1, name: 'Anderson' },
+  { id: 2, name: 'Helena' },
+  { id: 3, name: 'Elaine' },
+  { id: 4, name: 'Ana' },
+  { id: 5, name: 'Bárbara' },
+  { id: 6, name: 'Allysson' },
+  { id: 7, name: 'Eduardo' },
+  { id: 8, name: 'Arthur' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
